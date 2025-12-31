8b35b3cfc41b98f1dbeaaffc00854172c8394afe
DPayne25 <32944319+DPayne25@users.noreply.github.com>
2025-12-31 02:11:50 -0500

    Ensure structure and error ridding in ingestion engine: cot data fetch portion

--- src/ingestion/csv_rename.rs
+++ src/ingestion/csv_rename.rs
@@ -1,27 +1,19 @@
 use std::{fs, format, error::Error};
-use csv::Reader;
 
 pub fn rename_csv() -> Result<(), Box<dyn Error>> {
-    let mut reader = csv::Reader::from_path("data/raw/cot/date-cot_report.csv");
+    let path = "data/raw/cot/date-cot_report.csv";
+    let mut reader = csv::Reader::from_path(path)?;
 
-    for (row, result) in reader.records().enumerate() {
-        let record = result?;
+    let record = reader.records().nth(1).ok_or("Error: No second row found")??;
 
-         // Locate the second row (index 1). Second line incase future update includes headers
-        if row == 1 {
-            // Date value is in third column (index 2)
-            let cot_date = &record[2]; 
-        }
-        else {
-            println!("Error: Date not found in expected row.");
-        }
-        break;
-        }
+    let cot_date = record.get(2).ok_or("Error: No date found in the third column")?;
 
-    fs::rename("data/raw/cot/date-cot_report.csv", format!("data/raw/cot/{}-cot_report.csv", cot_date));
+    let new_path = format!("data/raw/cot/{}-cot_report.csv", cot_date);
+
+    fs::rename(path, &new_path)?;
 
     // Cofirm rename
-    println!("Successfully renamed file to {}-cot_report.csv", cot_date); 
+    println!("Successfully renamed file to {}", new_path); 
 
     Ok(())
 }
