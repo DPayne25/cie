de18b4e0cfbfd4892d49533878386f11732b75a7
DakotaJPayne <93561624+DakotaJPayne@users.noreply.github.com>
2025-12-31 00:13:11 -0500

    Add ingestion files to src/ingestion/mod.rs so that it is callable in src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+mod ingestion;
+
 use std::fs;
+
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://www.cftc.gov/dea/newcot/FinFutWk.txt")
-        .await?
-        .text()
-        .await?;
-    println!("{}", resp);
-    fs::write("data/raw/cot/date-cot_report.csv", resp)?;
+  
+
     Ok(())
 } 
