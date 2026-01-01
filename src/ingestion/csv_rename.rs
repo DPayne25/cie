3ffaeeb3204a1d18734956246a2dfbc8f2784c15
DakotaJPayne <93561624+DakotaJPayne@users.noreply.github.com>
2026-01-01 00:41:30 -0500

    ensure working model check point

--- src/ingestion/csv_rename.rs
+++ src/ingestion/csv_rename.rs
@@ -1,14 +1,14 @@
 use std::{fs, format, error::Error};
 
 pub fn rename_csv() -> Result<(), Box<dyn Error>> {
-    let path = "data/raw/cot/date-cot_report.csv";
+    let path = "data/raw/cot/date-raw_cot_report.csv";
     let mut reader = csv::Reader::from_path(path)?;
 
     let record = reader.records().nth(1).ok_or("Error: No second row found")??;
 
     let cot_date = record.get(2).ok_or("Error: No date found in the third column")?;
 
-    let new_path = format!("data/raw/cot/{}-cot_report.csv", cot_date);
+    let new_path = format!("data/raw/cot/{}-RawCOTReport.csv", cot_date);
 
     fs::rename(path, &new_path)?;
 
