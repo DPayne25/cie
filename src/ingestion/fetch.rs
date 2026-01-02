3e0f15704d042eae14e66c9e83cdf7705bfc3c10
DakotaJPayne <93561624+DakotaJPayne@users.noreply.github.com>
2026-01-01 21:43:13 -0500

    Progress on fetch_fx_price_data function

--- src/ingestion/fetch.rs
+++ src/ingestion/fetch.rs
@@ -1,5 +1,9 @@
-use std::fs;
-
+use std::{fs, env};
+use reqwest;
+#[allow(unused_imports)]
+use reqwest::{RequestBuilder, Response, Client, Request};
+#[allow(unused_imports)]
+use chrono;
 
 pub async fn fetch_cot_data() -> Result<(), Box<dyn std::error::Error>> {
     
@@ -15,7 +19,30 @@ pub async fn fetch_cot_data() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 } 
 
-//pub async fn fetch_fx_price_data() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn fetch_fx_price_data(
+    insturment: &str, 
+    from_date: &str, 
+    granularity: &str, 
+    price_type: &str
+) -> Result<(), Box<dyn std::error::Error>> {
 
-//    let fx_data = ;
-//}
+    let api_key = env::var(OANDA_API_KEY).map_err(|_| "OANDA_API_KEY not set in .env file")?;
+
+    let client = reqwest::Client::new();
+
+        let get_data = client.get(&format!("https://api-fxtrade.oanda.com/v3/instruments/{}/candles", instrument))
+        .header("Authorization", format!("Bearer {}", api_key))
+        .query(&[
+            ("price", price_type),
+            ("from", from_date),
+            ("granularity", granularity)
+        ])
+        .send()
+        .await?
+        .error_for_status()?;
+
+    let fx_data = get_data.text().await?;
+    println!("{}", fx_data);
+
+    Ok(())
+} 
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   
     ingestion::fetch::fetch_cot_data().await?;
 
+    ingestion::fetch::fetch_fx_price_data("EUR_USD", "2016-01-01T15%3A00%3A00.000000000Z", "D", "M").await?;
+
     ingestion::csv_logic::csv_validate()?;  
 
     ingestion::csv_logic::csv_process_raw_cot()?; 
