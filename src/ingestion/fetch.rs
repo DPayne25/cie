c20a061d4338e85cdc36777c058c3d278aa6f594
DPayne25 <32944319+DPayne25@users.noreply.github.com>
2026-01-03 15:46:30 -0500

    Troubleshoot fxoanda functionality

--- src/ingestion/fetch.rs
+++ src/ingestion/fetch.rs
@@ -1,5 +1,8 @@
 use std::{fs, env};
-use reqwest;
+use chrono::{DateTime, Utc, TimeZone};
+use dotenvy::dotenv;
+use fxoanda::*;
+use reqwest::{ClientBuilder, Client, Url};
 
 pub async fn fetch_cot_data() -> Result<(), Box<dyn std::error::Error>> {
     
@@ -15,29 +18,36 @@ pub async fn fetch_cot_data() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 } 
 
-pub async fn fetch_fx_price_data(
+pub fn fetch_fx_price_data(
     instrument: &str, 
-    from_date: &str, 
+    //from_date: DateTime<Utc>, Add loop to input into `dt` below #todo so input varies on input
     granularity: &str, 
     price_type: &str
 ) -> Result<(), Box<dyn std::error::Error>> {
+    let dt: DateTime<Utc> = Utc.with_ymd_and_hms(2016, 1, 1, 15, 0, 0).unwrap(); //todo line 23
 
-    let api_key = env::var(OANDA_API_KEY).map_err(|_| "OANDA_API_KEY not set in .env file")?;
+    let oanda_time_format = dt.to_rfc3339();
 
-    let client = reqwest::Client::new();
+    dotenv().ok();
 
-    let get_data = client.get(&format!("https://api-fxtrade.oanda.com/v3/instruments/{}/candles", instrument))
-        .header("Authorization", format!("Bearer {}", api_key))
-        .query(&[
-            ("price", price_type),
-            ("from", from_date),
-            ("granularity", granularity)
-        ])
-        .send()
-        .await?
-        .error_for_status()?;
+    let api_key = env::var("OANDA_API_KEY").map_err(|_| "OANDA_API_KEY not set in .env file")?;
 
-    let fx_data = get_data.text().await?;
+    let client= fxoanda::Client {
+        host: "api-fxtrade.oanda.com".to_string(),
+        reqwest: reqwest::Client::new(),
+        authentication: String::from(api_key)
+    };
+
+    let mut get_data = fxoanda::GetInstrumentCandlesRequest::new()
+        .with_instrument(instrument.to_string())
+        .with_from(from_date)
+        .with_granularity(CandlestickGranularity::from_str(granularity)?)
+        .with_price(price_type.to_string())
+        .remote(&client).await;
+
+
+
+    let fx_data = get_data.json().await?;
     
     fs::write(format!("data/raw/fx_prices/{}_{}_{}_RawFXPriceData.json", instrument, from_date, granularity), fx_data)?;
 
