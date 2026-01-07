292a213ade6fc2093265023f85b01ae93f463833
DakotaJPayne <93561624+DakotaJPayne@users.noreply.github.com>
2026-01-07 03:33:00 -0500

    Make all historical pair data download asynchronously

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 mod ingestion;
-//use chrono::{DateTime, Utc};
+use futures::future::join_all;
 
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
+    let fx_pairs  = vec![
+        "EUR_USD", 
+        "USD_JPY", 
+        "GBP_USD", 
+        "AUD_USD", 
+        "USD_CAD", 
+        "USD_CHF", 
+        "NZD_USD"];
+
     let client = reqwest::Client::new();
     //let start_date = "2016-01-01T15:00:00.000000000Z".parse::<DateTime<Utc>>().unwrap();
   
     ingestion::fetch::fetch_cot_data().await?;
 
-    ingestion::fetch::fetch_fx_price_data(&client, "EUR_USD", "D", "M").await?;
+    let fetch_all_fx_data = fx_pairs.iter().map(|pair| {
+        ingestion::fetch::fetch_fx_price_data(&client, pair, "D", "M")
+    });
+
+    join_all(fetch_all_fx_data).await;
 
     ingestion::csv_logic::csv_validate()?;  
 
