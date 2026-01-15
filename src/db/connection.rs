8dcbaa5eeb6f9d031827cc26955bb2fddd9ed6dd
DPayne25 <32944319+DPayne25@users.noreply.github.com>
2026-01-15 00:25:52 -0500

    Start insert raw data file

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -1,10 +1,16 @@
 use std::env;
-use sqlx::{Pool, Postgres};
+use sqlx::PgPool;
 
-pub async fn connect_db() -> Result<Pool<Postgres>, Box<dyn std::error::Error>> {
+pub async fn db_access() -> Result<String, Box<dyn std::error::Error>> {
     dotenvy::from_filename("./config/database.env").ok();
     let db_url: String = env::var("DATABASE_URL")?;
-    let db_pool: Pool<Postgres> = sqlx::postgres::PgPoolOptions::new().connect(&db_url).await?;
+    
+    Ok(db_url)
+}
+
+pub async fn connect_db() -> Result<PgPool, Box<dyn std::error::Error>> {
+    let db_url = db_access().await?;
+    let db_pool: PgPool = sqlx::postgres::PgPoolOptions::new().connect(&db_url).await?;
     sqlx::migrate!("./sql/schema").run(&db_pool).await?;
 
     Ok(db_pool)
---
+++ src/db/insert_raw.rs
@@ -0,0 +1,17 @@
+use sqlx::{PgPool};
+
+pub async fn insert_raw_cot(pool: &PgPool, data: &Vec) -> Result<(), Box<dyn std::error::Error>>{
+
+    let db_url: String = std::env::var("DATABASE_URL")?;
+    let mut data = Vec::new();
+
+    for report in rows{
+        data = sqlx::query!("INSERT INTO ").execute(pool).await?;
+
+    }
+    
+
+    println!("{}", data);
+
+    Ok(())
+}
