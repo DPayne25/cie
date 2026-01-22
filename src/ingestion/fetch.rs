a740ede905e91eb56ed6a2d8c820ce2440dbdb40
DPayne25 <32944319+DPayne25@users.noreply.github.com>
2026-01-22 02:31:17 -0500

    Update fetch with granularity varible from an enum

--- src/ingestion/fetch.rs
+++ src/ingestion/fetch.rs
@@ -41,7 +41,7 @@ pub async fn fetch_fx_price_data(
     let get_data = fxoanda::GetInstrumentCandlesRequest::new()
         .with_instrument(instrument.to_string())
         .with_from(dt)
-        .with_granularity(fxoanda::CandlestickGranularity::D)
+        .with_granularity(granularity.into())
         .with_price(price_type.to_string())
         .with_count(5000)
         .remote(&oanda_client).await;
@@ -58,3 +58,35 @@ pub async fn fetch_fx_price_data(
 
     Ok(())
 } 
+
+pub enum CandlestickGranularity {
+    S5, S10, S15, S30, M1, M2, M4, M5, M10, M15, M30, H1, H2, H3, H4, H6, H8, H12, D, W, M
+}
+
+impl From<CandlestickGranularity> for fxoanda::CandlestickGranularity {
+    fn from(g: CandlestickGranularity) -> Self {
+        match g {
+            CandlestickGranularity::S5 => fxoanda::CandlestickGranularity::S5,
+            CandlestickGranularity::S10 => fxoanda::CandlestickGranularity::S10,
+            CandlestickGranularity::S15 => fxoanda::CandlestickGranularity::S15,
+            CandlestickGranularity::S30 => fxoanda::CandlestickGranularity::S30,
+            CandlestickGranularity::M1 => fxoanda::CandlestickGranularity::M1,
+            CandlestickGranularity::M2 => fxoanda::CandlestickGranularity::M2,
+             CandlestickGranularity::M4 => fxoanda::CandlestickGranularity::M4,
+            CandlestickGranularity::M5 => fxoanda::CandlestickGranularity::M5,
+            CandlestickGranularity::M10 => fxoanda::CandlestickGranularity::M10,
+            CandlestickGranularity::M15 => fxoanda::CandlestickGranularity::M15,
+            CandlestickGranularity::M30 => fxoanda::CandlestickGranularity::M30,
+            CandlestickGranularity::H1 => fxoanda::CandlestickGranularity::H1,
+            CandlestickGranularity::H2 => fxoanda::CandlestickGranularity::H2,
+            CandlestickGranularity::H3 => fxoanda::CandlestickGranularity::H3,
+            CandlestickGranularity::H4 => fxoanda::CandlestickGranularity::H4,
+            CandlestickGranularity::H6 => fxoanda::CandlestickGranularity::H4,
+            CandlestickGranularity::H8 => fxoanda::CandlestickGranularity::H8,
+            CandlestickGranularity::H12 => fxoanda::CandlestickGranularity::H12,
+            CandlestickGranularity::D => fxoanda::CandlestickGranularity::D,
+            CandlestickGranularity::W => fxoanda::CandlestickGranularity::W,
+            CandlestickGranularity::M => fxoanda::CandlestickGranularity::M
+        }
+    }
+}
