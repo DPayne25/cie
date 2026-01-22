4c0ff194f916fb1668ce4adaa2b37570af935420
DPayne25 <32944319+DPayne25@users.noreply.github.com>
2026-01-22 02:05:47 -0500

    Fix Issue: Redundant I/O & Algorithmic Complexity (Ensure)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod ingestion;
 use futures::future::join_all;
-use std::path::Path;
+use std::{path::Path, error::Error};
 
 
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn Error>> {
 
     let fx_pairs  = vec![
         "EUR_USD", 
@@ -21,7 +21,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
   
     ingestion::fetch::fetch_cot_data().await?;
-
     let fetch_all_fx_data = fx_pairs.iter().map(|pair| {
         ingestion::fetch::fetch_fx_price_data(
             &client,
