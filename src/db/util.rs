94e6de0958a435057c49a05d819dbe22798681ff
DPayne25 <32944319+DPayne25@users.noreply.github.com>
2026-01-27 04:49:56 -0500

    Alter for db/util so that the output possibly returns NULL for more versatilty in other areas of the project

--- src/db/util.rs
+++ src/db/util.rs
@@ -3,12 +3,12 @@ use sqlx::PgPool;
 use chrono::{DateTime, Utc};
 use std::error::Error;
 
-pub async fn get_latest_timestamp(date: DateTime<Utc>, pool: &PgPool, symbol: &str) -> Result<DateTime<Utc>, Box<dyn Error>> {
+pub async fn get_latest_timestamp(date: DateTime<Utc>, pool: &PgPool, symbol: &str) -> Result<DateTime<Utc>>, Box<dyn Error>> {
     
-    let latest_date: DateTime<Utc> = sqlx::query_scalar!(
+    let latest_date: Option<DateTime<Utc>> = sqlx::query_scalar!(
         "SELECT MAX(date) FROM raw_fx_prices WHERE symbol = $1", symbol
     )
     .fetch_optional(pool)
     .await?;
-    Ok(latest_date.unwrap_or(date))
+    Ok(latest_date)
 }
