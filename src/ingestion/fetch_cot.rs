02e551ebedccfefa3acbfef6ec4261fd3ff3afc4
DakotaJPayne <93561624+DakotaJPayne@users.noreply.github.com>
2026-02-18 17:38:10 -0500

    Seperate fetch_fx_price_data from fetch.rs to clarify cot and fxprice fetch

---
+++ src/ingestion/fetch_cot.rs
@@ -0,0 +1,32 @@
+use std::{fs::{self, File}, error::Error, path::Path, io};
+use zip::ZipArchive;
+
+
+
+pub async fn fetch_cot_data(year: i64) -> Result<String, Box<dyn Error>> {
+    
+    let url = format!("https://www.cftc.gov/files/dea/history/fut_fin_txt_{}.zip", year);
+
+    println!("• Fetching COT data from: {}", url);
+
+    let response_bytes = reqwest::get(&url).await?.bytes().await?;
+    let cursor = io::Cursor::new(response_bytes);
+    let mut archive = ZipArchive::new(cursor)?;
+
+    let mut file_in_zip = archive.by_name("FinFutWk.txt")
+        .map_err(|_| format!("Could not find 'FinFutWk.txt' in archive for year {}", year))?;
+
+    let out_dir = Path::new("data/raw/cot");
+    fs::create_dir_all(out_dir)?;
+
+    let temp_path_str = "data/raw/cot/date-RawCOTReport.csv";
+    let out_path = Path::new(temp_path_str);
+    let mut outfile = File::create(&out_path)?;
+
+    io::copy(&mut file_in_zip, &mut outfile)?;
+
+    println!("• Successfully extracted and saved raw COT data to: {:?}", out_path);
+
+    Ok(temp_path_str.to_string())
+} 
+
