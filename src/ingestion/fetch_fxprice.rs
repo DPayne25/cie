28a1206b09ea1c7f43eea0e716f108a0c97355f6
DPayne25 <32944319+DPayne25@users.noreply.github.com>
2026-02-18 17:38:10 -0500

    Seperate fetch_fx_price_data from fetch.rs to clarify cot and fxprice fetch

--- src/ingestion/fetch_fxprice.rs
+++ src/ingestion/fetch_fxprice.rs
@@ -1,37 +1,8 @@
-use std::{fs::{self, File}, env, error::Error, path::Path, io};
+use reqwest;
+use std::{fs, env, error::Error};
 use chrono::{DateTime, Utc, TimeZone};
 use dotenvy::dotenv;
 use fxoanda;
-use zip::ZipArchive;
-
-
-
-pub async fn fetch_cot_data(year: i64) -> Result<String, Box<dyn Error>> {
-    
-    let url = format!("https://www.cftc.gov/files/dea/history/fut_fin_txt_{}.zip", year);
-
-    println!("• Fetching COT data from: {}", url);
-
-    let response_bytes = reqwest::get(&url).await?.bytes().await?;
-    let cursor = io::Cursor::new(response_bytes);
-    let mut archive = ZipArchive::new(cursor)?;
-
-    let mut file_in_zip = archive.by_name("FinFutWk.txt")
-        .map_err(|_| format!("Could not find 'FinFutWk.txt' in archive for year {}", year))?;
-
-    let out_dir = Path::new("data/raw/cot");
-    fs::create_dir_all(out_dir)?;
-
-    let temp_path_str = "data/raw/cot/date-RawCOTReport.csv";
-    let out_path = Path::new(temp_path_str);
-    let mut outfile = File::create(&out_path)?;
-
-    io::copy(&mut file_in_zip, &mut outfile)?;
-
-    println!("• Successfully extracted and saved raw COT data to: {:?}", out_path);
-
-    Ok(temp_path_str.to_string())
-} 
 
 
 pub async fn fetch_fx_price_data(
@@ -40,7 +11,7 @@ pub async fn fetch_fx_price_data(
     //from_date: DateTime<Utc>, 
     granularity: &str, 
     price_type: &str
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<(), Box<dyn Error>> {
     let dt: DateTime<Utc> = Utc.with_ymd_and_hms(2016, 1, 1, 0, 0, 0).unwrap(); //todo line 23
 
     dotenv().ok();
--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
      // let current_year: i32 = chrono::Utc::now().year();
 
-    ingestion::fetch::fetch_cot_data(2025).await?;
+    ingestion::fetch_cot::fetch_cot_data(2025).await?;
   
     let fetch_all_fx_data = fx_pairs.iter().map(|pair| {
-        ingestion::fetch::fetch_fx_price_data(&client, 
+        ingestion::fetch_fxprice::fetch_fx_price_data(&client, 
             pair, 
             "D", 
             "M")
