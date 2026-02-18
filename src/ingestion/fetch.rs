dfb673ae0dd54edd96453726eb2790b4a2baebd7
DakotaJPayne <93561624+DakotaJPayne@users.noreply.github.com>
2026-02-18 16:58:55 -0500

    Prepare for debug/refactor of fetch.rs

--- src/ingestion/fetch.rs
+++ src/ingestion/fetch.rs
@@ -1,106 +1,12 @@
-use std::{fs, env, error::Error, str::FromStr, num::ParseIntError, io::{self, Cursor}};
-use chrono::{DateTime, Utc, TimeZone, NaiveDate};
+use std::{fs::{self, File}, env, error::Error, str::FromStr, path::Path, io};
+use chrono::{DateTime, Utc, TimeZone};
 use dotenvy::dotenv;
 use fxoanda;
-use csv::ReaderBuilder;
-use rust_decimal::Decimal;
-use serde::{Serialize, Deserialize};
 use zip::ZipArchive;
 
 
-#[derive(Deserialize, Serialize, Debug)]
-struct RawCot{
-    #[serde(rename = "Market_and_Exchange_Names")]
-    market_name: String,
-    #[serde(rename = "Report_Date_as_YYYY-MM-DD")]
-    report_date: String,
-    #[serde(rename = "CFTC_Contract_Market_Code")]
-    tff_code: String,
-    #[serde(rename = "Open_Interest_All")]
-    open_interest_all: String,
-    #[serde(rename = "Dealer_Positions_Long_All")]
-    dealer_long: String,
-    #[serde(rename = "Dealer_Positions_Short_All")]
-    dealer_short: String,
-    #[serde(rename = "Dealer_Positions_Spread_All")]
-    dealer_spread: String,
-    #[serde(rename = "Asset_Mgr_Positions_Long_All")]
-    asset_mgr_long: String,
-    #[serde(rename = "Asset_Mgr_Positions_Short_All")]
-    asset_mgr_short: String,
-    #[serde(rename = "Asset_Mgr_Positions_Spread_All")]
-    asset_mgr_spread: String,
-    #[serde(rename = "Lev_Money_Positions_Long_All")]
-    lev_money_long: String,
-    #[serde(rename = "Lev_Money_Positions_Short_All")]
-    lev_money_short: String,
-    #[serde(rename = "Lev_Money_Positions_Spread_All")]
-    lev_money_spread: String,
-    #[serde(rename = "Other_Rept_Positions_Long_All")]
-    other_rept_long: String,
-    #[serde(rename = "Other_Rept_Positions_Short_All")]
-    other_rept_short: String,
-    #[serde(rename = "Other_Rept_Positions_Spread_All")]
-    other_rept_spread: String,
-}
 
-#[derive(Debug,  Serialize)]
-struct CotReport {
-    market_name: String,
-    report_date: NaiveDate,
-    tff_code: String,
-    open_interest_all: Decimal,
-    dealer_long: Decimal,
-    dealer_short: Decimal,
-    dealer_spread: Decimal,
-    asset_mgr_long: Decimal,
-    asset_mgr_short: Decimal,
-    asset_mgr_spread: Decimal,
-    lev_money_long: Decimal,
-    lev_money_short: Decimal,
-    lev_money_spread: Decimal,
-    other_rept_long: Decimal,
-    other_rept_short: Decimal,
-    other_rept_spread: Decimal,
-}
-
-impl TryFrom<RawCot> for CotReport {
-    type Error = Box<dyn Error>;
-
-    fn try_from(raw: RawCot) -> Result<Self, Self::Error> {
-        Ok(CotReport {
-            market_name: raw.market_name,
-            report_date: NaiveDate::parse_from_str(&raw.report_date, "%Y-%m-%d")?,
-            tff_code: raw.tff_code,
-            open_interest_all: raw.open_interest_all.parse::<Decimal>()?,
-            dealer_long: Decimal::from_str(&raw.dealer_long.replace(',', ""))?,
-            dealer_short: Decimal::from_str(&raw.dealer_short.replace(',', ""))?,
-            dealer_spread: Decimal::from_str(&raw.dealer_spread.replace(',', ""))?,
-            asset_mgr_long: Decimal::from_str(&raw.asset_mgr_long.replace(',', ""))?,
-            asset_mgr_short: Decimal::from_str(&raw.asset_mgr_short.replace(',', ""))?,
-            asset_mgr_spread: Decimal::from_str(&raw.asset_mgr_spread.replace(',', ""))?,
-            lev_money_long: Decimal::from_str(&raw.lev_money_long.replace(',', ""))?,
-            lev_money_short: Decimal::from_str(&raw.lev_money_short.replace(',', ""))?,
-            lev_money_spread: Decimal::from_str(&raw.lev_money_spread.replace(',', ""))?,
-            other_rept_long: Decimal::from_str(&raw.other_rept_long.replace(',', ""))?,
-            other_rept_short: Decimal::from_str(&raw.other_rept_short.replace(',', ""))?,
-            other_rept_spread: Decimal::from_str(&raw.other_rept_spread.replace(',', ""))?,
-        })
-
-    }
-}
-
-/*
-pub fn parse_cftc_numbers(value: &str) -> Result<i64, ParseIntError> { 
-    if value.contains(',') {
-        value.replace(",", "").trim().parse()
-    } else {
-        value.trim().parse()
-    }
-}
-*/
-
-pub async fn fetch_cot_data(year: i64) -> Result<(), Box<dyn Error>> {
+pub async fn fetch_cot_data(year: i64) -> Result<String, Box<dyn Error>> {
     
     // Using bytes in preparation for a future hard disk bypass version
     let cot_request = reqwest::get(format!("https://www.cftc.gov/files/dea/history/fut_fin_txt_{}.zip", year))
